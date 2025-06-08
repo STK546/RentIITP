2bc6735a7c7b1a7974966477057fa35563b7af21
Krishal23 <kanhakesharwani23@gmail.com>
2025-06-09 01:44:12 +0530

    db fixes

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -3,11 +3,22 @@ import { config } from 'dotenv';
 
 config(); 
 
+console.log('DB config:', {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_DATABASE,
+});
+
+
 const pool = createPool({
     host: process.env.DB_HOST || 'localhost',
+    port: parseInt(process.env.DB_PORT) || 3306,  // Add this line
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
+    ssl: {
+        rejectUnauthorized: false // or true depending on your server config
+    },
     waitForConnections: true,
     connectionLimit: parseInt(process.env.DB_CONNECTION_LIMIT) || 10,
     queueLimit: 0
